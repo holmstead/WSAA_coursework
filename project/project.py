51771f88aae7df7a2972caec19dd57375076d585
comput <mat.holmes@outlook.com>
2025-03-24 19:48:03 +0000

    add html template rendering

--- project/project.py
+++ project/project.py
@@ -1,10 +1,13 @@
-from flask import Flask, url_for, request, redirect, abort
+from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path='', static_folder='staticpages')
+app = Flask(__name__) # Flask constructor
 
+# Bind the index() funtion to the ‘/’ URL (homepage) 
 @app.route('/')
 def index():
-    return "test"
+    # Render the template 'index.html' from the 'templates' folder
+    return render_template('index.html')
 
 if __name__ == "__main__":
+    # Start the application (in debug mode)
     app.run(debug=True)
