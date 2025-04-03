6a7a15ae283cd6b1f6bfd076e4400ed8a0f5bf8b
comput <mat.holmes@outlook.com>
2025-04-03 19:15:46 +0100

    added get all action

--- project/project.py
+++ project/project.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
 
-app = Flask(__name__) # Flask constructor
+app = Flask(__name__, static_url_path='', static_folder='.') # Flask constructor
+# makes static files accessible at the root URL (/) instead of /static
 
 # Bind the index() funtion to the ‘/’ URL (homepage) 
 @app.route('/')
 def index():
     # Render the template 'index.html' from the 'templates' folder
     return render_template('index.html')
+
+
+
+@app.route('/recipies', methods=['GET'])
+def getall():
+    return "get all"
+
 
 if __name__ == "__main__":
     # Start the application (in debug mode)
