8787d09ca7e3258778ea00f1bb0615e9c33b9efb
comput <mat.holmes@outlook.com>
2025-04-03 20:36:02 +0100

    finished skeleton mapping

--- project/project.py
+++ project/project.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__, static_url_path='', static_folder='.') # Flask constructor
 # makes static files accessible at the root URL (/) instead of /static
@@ -6,15 +7,34 @@ app = Flask(__name__, static_url_path='', static_folder='.') # Flask constructor
 # Bind the index() funtion to the ‘/’ URL (homepage) 
 @app.route('/')
 def index():
-    # Render the template 'index.html' from the 'templates' folder
+    # Render the template 'index.html' from the 'templates' folder. render_template is good for dynamic content, so not using staticpages
     return render_template('index.html')
 
-
-
-@app.route('/recipies', methods=['GET'])
+# curl "http://127.0.0.1:5000/recipies" 
+@app.route('/recipies', methods=['GET']) # Action: Get All
 def getall():
-    return "get all"
+    return "\nget all\n"
 
+# curl "http://127.0.0.1:5000/recipies/123"
+@app.route('/recipies/<int:id>', methods=['GET']) # Action: Get. <int:id> is a variable, URL looks like ~/recipies/234
+def findbyid(id):
+    return "\nfind by id\n"
+
+# curl "http://127.0.0.1:5000/recipies"
+@app.route('/recipies', methods=['POST']) # Action: Create
+def create():
+    # read json from the body
+    json_string = request.json
+    return f"\ncreate {json_string}\n"
+
+@app.route('/recipies/<int:id>', methods=['PUT']) # Action: Update
+def update(id):
+    json_string = request.json
+    return f"\nupdate {id}{json_string}\n"
+
+@app.route('/recipies/<int:id>d', methods=['DELETE']) # Action: Delete
+def delete(id):
+    return "\ndelete id\n"
 
 if __name__ == "__main__":
     # Start the application (in debug mode)
