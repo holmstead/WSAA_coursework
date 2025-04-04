997c5fe23bae43a161b36f4216c89834eedb3bb4
comput <mat.holmes@outlook.com>
2025-04-04 19:17:30 +0100

    add basic data access object file

---
+++ project/recipeDAO.py
@@ -0,0 +1,19 @@
+# Return all recipes in the database table
+def getall():
+    return [{}]
+
+# Return one recipe
+def findById(id):
+    return {}
+
+# Insert a recipe into the database
+def create(recipe):
+    return recipe
+
+# Update a recipe
+def update(id, recipe):
+    return recipe
+
+# Deletes a recipe
+def delete(id):
+    return True
