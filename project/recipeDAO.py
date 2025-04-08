53dec681b856bd3d1d2d75a54cae2fe7b617dfac
comput <mat.holmes@outlook.com>
2025-04-08 19:39:59 +0100

    create class object in DAO file

--- project/recipeDAO.py
+++ project/recipeDAO.py
@@ -1,26 +1,26 @@
 import mysql.connector # for connecting python to the mysql database
 
-# Create a connection to the MySQL server.
-connection = mysql.connector.connect(
-    #TODO put in config file
-    host="localhost",
-    user="root",
-    password="root"
-    )
+# create recipeDAO class
+class RecipeDAO:
+    # constructor
+    def __init__(self):
+        # connect to the database
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root",
+            database="recipes"
+        )
+        # creata a cursor (for executing SQL commands)
+        self.cursor = self.db.cursor(dictionary=True) # return as a dictionary
 
-# Create a cursor to execute queries
-mycursor = connection.cursor()
-
-# example query
-sql= "SHOW DATABASES"
-
-# Execute sql queyr
-mycursor.execute(sql)
+    def get_all(self):
+        # execute sql query - return all rows from the recipe table
+        self.cursor.execute("SELECT * FROM recipe")
+        # retrieve all the results and return them
+        return self.cursor.fetchall()
 
 
-# Return all recipes in the database table
-def getall():
-    return [{}]
 
 # Return one recipe
 def findById(id):
@@ -37,9 +37,3 @@ def update(id, recipe):
 # Deletes a recipe
 def delete(id):
     return True
-
-
-
-# Close the cursor and connection
-mycursor.close()
-connection.close()
