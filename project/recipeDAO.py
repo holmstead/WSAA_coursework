3af809bd9bc4e75b25876871ae56ea71967ac73b
comput <mat.holmes@outlook.com>
2025-04-10 21:25:38 +0100

    updated find_by_id() method

--- project/recipeDAO.py
+++ project/recipeDAO.py
@@ -6,10 +6,12 @@ class RecipeDAO:
     # constructor
     def __init__(self):
         # connect to the database
+        # **db_config unpacks the dictionary into keyword arguments
         self.db = mysql.connector.connect(**db_config) # use the config from dbconfig.py
         # creata a cursor (for executing SQL commands)
         self.cursor = self.db.cursor(dictionary=True) # return as a dictionary
 
+    # Return all recipes
     def get_all(self):
         # execute sql query - return all rows from the recipe table
         self.cursor.execute("SELECT * FROM recipe")
@@ -17,19 +19,21 @@ class RecipeDAO:
         return self.cursor.fetchall()
 
 
+    # Return one recipe
+    def find_by_id(self, id):
+        sql = "SELECT * FROM recipe WHERE id = %s"
+        self.cursor.execute(sql, (id,))
+        result = self.cursor.fetchone()
+        return result
 
-# Return one recipe
-def findById(id):
-    return {}
+    # Insert a recipe into the database
+    def create(recipe):
+        return recipe
 
-# Insert a recipe into the database
-def create(recipe):
-    return recipe
+    # Update a recipe
+    def update(id, recipe):
+        return recipe
 
-# Update a recipe
-def update(id, recipe):
-    return recipe
-
-# Deletes a recipe
-def delete(id):
-    return True
+    # Deletes a recipe
+    def delete(id):
+        return True
